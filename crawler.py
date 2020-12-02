53db2cd6309f59a561670818a96655e0d53613fc
Daniele Albanese <daniele.albanese98@gmail.com>
2020-12-02 02:31:19 +0100

    crawler.py -> Added function get_metadata

--- crawler.py
+++ crawler.py
@@ -15,3 +15,33 @@ def get_extensions_urls(last_page):
             href_list.append(cons.BASE_URL + href.get('href'))
 
     return href_list
+
+
+def get_metadata(extension_links):
+    metadata_list = []
+
+    for link in extension_links:
+        html = requests.get(link)
+
+        page = BeautifulSoup(html.content, "html.parser")
+
+        name = page.find("h1", class_="AddonTitle").text
+        author = page.find("span", class_="AddonTitle-author").text
+
+        metadata = {
+            'name': name.replace(author, "").strip(),
+            'author': author.replace("by ", "").strip(),
+            'number_of_users': int(page.find("dd", class_="MetadataCard-content").text.replace(",", "")),
+            'stars_average': float(page.find('div', class_="AddonMeta-rating-title").text.replace(" Stars", "")),
+            'comment_link': cons.BASE_URL + page.find('a', class_='AddonMeta-reviews-title-link').get('href')
+        }
+
+        metadata_list.append(metadata)
+
+    return metadata_list
+
+
+
+# links = ["https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search", "https://addons.mozilla.org/en-US/firefox/addon/video-downloadhelper/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search"]
+#
+# print(get_metadata(links))
